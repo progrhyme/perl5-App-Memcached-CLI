fd72b9cc2e1b96f8e3684bdc41d53b9f95eaf4cc
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-21 13:02:43 +0900

    minil new App-Memcached-Monitor

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Memcached::Monitor - It's new $module
+
+# SYNOPSIS
+
+    use App::Memcached::Monitor;
+
+# DESCRIPTION
+
+App::Memcached::Monitor is ...
+
+# LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+YASUTAKE Kiyoshi <yasutake.kiyoshi@gmail.com>
---
+++ lib/App/Memcached/Monitor.pm
@@ -0,0 +1,39 @@
+package App::Memcached::Monitor;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Memcached::Monitor - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Memcached::Monitor;
+
+=head1 DESCRIPTION
+
+App::Memcached::Monitor is ...
+
+=head1 LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
