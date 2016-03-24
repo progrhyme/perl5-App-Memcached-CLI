1db45744b26f0deed38f84b0dfa9edbd9e24c615
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-25 02:15:30 +0900

    Use Help.pm for help text

--- lib/App/Memcached/CLI/Main.pm
+++ lib/App/Memcached/CLI/Main.pm
@@ -13,6 +13,7 @@ use Term::ReadLine;
 
 use App::Memcached::CLI;
 use App::Memcached::CLI::DataSource;
+use App::Memcached::CLI::Help;
 use App::Memcached::CLI::Item;
 use App::Memcached::CLI::Util ':all';
 
@@ -165,114 +166,8 @@ sub help {
     my $self    = shift;
     my $command = shift || q{};
 
-    my @command_info = (
-        +{
-            command => 'help',
-            summary => 'Show help (this)',
-        },
-        +{
-            command => 'version',
-            summary => 'Show server version',
-        },
-        +{
-            command => 'quit',
-            summary => 'Exit',
-        },
-        +{
-            command => 'display',
-            summary => 'Display slabs info',
-        },
-        +{
-            command => 'stats',
-            summary => 'Show stats',
-            description => <<'EODESC',
-Usage:
-    > stats [<REGEXP>]
-    > stats            # show all stats
-    > stats (hit|miss) # filter by REGEXP
-EODESC
-        },
-        +{
-            command => 'settings',
-            summary => 'Show settings',
-            description => <<'EODESC',
-Usage:
-    > settings [<REGEXP>]
-    > settings             # show all stats
-    > settings ^(lru|slab) # filter by REGEXP
-EODESC
-        },
-        +{
-            command => 'cachedump',
-            summary => 'Show cachedump of specified slab',
-            description => <<'EODESC',
-Usage:
-    > cachedump <CLASS> [<NUMBER>]
-    > cachedump 1 10
-    > cachedump 3     # show default number of items
-EODESC
-        },
-        +{
-            command => 'detaildump',
-            summary => 'Show detail dump',
-            description => <<'EODESC',
-Description:
-    Report statistics about data access using KEY prefix. The default separator
-    for prefix is ':'.
-    If you have not enabled reporting at Memcached start-up, run "detail on".
-    See man memcached(1) for details.
-EODESC
-        },
-        +{
-            command => 'detail',
-            summary => 'Enable/Disable detail dump',
-            description => <<'EODESC',
-Usage:
-    > detail on
-    > detail off
+    my @command_info = @App::Memcached::CLI::Help::COMMANDS_INFO;
 
-Description:
-    See "\h detaildump"
-EODESC
-        },
-        +{
-            command => 'get',
-            summary => 'Get data by KEY',
-            description => <<'EODESC',
-Usage:
-    > get <KEY>
-EODESC
-        },
-        +{
-            command => 'set',
-            summary => 'Set data by KEY VALUE',
-            description => <<'EODESC',
-Usage:
-    > set <KEY> <VALUE> [<EXPIRE> [<FLAGS>]]
-    > set mykey1 MyValue1
-    > set mykey2 MyValue2 0     # Never expires. Default
-    > set mykey3 MyValue3 120 1
-EODESC
-        },
-        +{
-            command => 'delete',
-            summary => 'Delete data by KEY',
-            description => <<'EODESC',
-Usage:
-    > delete <KEY>
-EODESC
-        },
-        +{
-            command => 'flush_all',
-            summary => 'Invalidate whole data',
-            description => <<'EODESC',
-Usage:
-    > flush_all [<DELAY>]
-    > flush_all           # Invalidate immediately
-    > flush_all 60        # Invalidate after 60 seconds
-EODESC
-        },
-    );
     my $body   = q{};
     my $space  = ' ' x 4;
 
