6395af545e18c368e0e0dec090837b2cf6f4f17c
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-24 12:40:09 +0900

    Checking in changes prior to tagging of version v0.3.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 631ab4c..1274346 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension App-Memcached-CLI
    
     {{$NEXT}}
    
    +v0.3.0 2016-03-24T03:40:05Z
    +
    +    - Feature: implement "detaildump" command for "stats detail dump"
    +    - Feature: implement "detail" command to toggle stats collection
    +
     v0.2.3 2016-03-24T01:26:45Z
    
         - Change: remove Encode::Guess from dependency. Decode ASCII text only when

--- lib/App/Memcached/CLI/Main.pm
+++ lib/App/Memcached/CLI/Main.pm
@@ -16,7 +16,7 @@ use App::Memcached::CLI::DataSource;
 use App::Memcached::CLI::Item;
 use App::Memcached::CLI::Util ':all';
 
-use version; our $VERSION = 'v0.2.3';
+use version; our $VERSION = 'v0.3.0';
 
 my %COMMAND2ALIASES = (
     help       => ['\h'],
