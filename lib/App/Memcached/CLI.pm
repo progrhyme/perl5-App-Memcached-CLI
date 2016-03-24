c3dd7ed64b5818c1155c4442817348650bdbe620
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-24 10:26:50 +0900

    Checking in changes prior to tagging of version v0.2.3.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5e95806..631ab4c 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension App-Memcached-CLI
    
     {{$NEXT}}
    
    +v0.2.3 2016-03-24T01:26:45Z
    +
    +    - Change: remove Encode::Guess from dependency. Decode ASCII text only when
    +      GET item.
    +
     v0.2.2 2016-03-23T13:22:39Z
    
         - Feature: implement "get" command in interactive mode to GET data of

--- lib/App/Memcached/CLI.pm
+++ lib/App/Memcached/CLI.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.008_001;
 
-use version; our $VERSION = 'v0.2.2';
+use version; our $VERSION = 'v0.2.3';
 
 our $DEBUG; # set by option
 
--- lib/App/Memcached/CLI/Item.pm
+++ lib/App/Memcached/CLI/Item.pm
@@ -6,7 +6,7 @@ use 5.008_001;
 
 use App::Memcached::CLI::Util ':all';
 
-use version; our $VERSION = 'v0.2.2';
+use version; our $VERSION = 'v0.2.3';
 
 my $DISPLAY_DATA_LENGTH = 320;
 
